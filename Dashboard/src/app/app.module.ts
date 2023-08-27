b83e491aa99fbe53804f32d80d1b8298e5de29ca
Akash Pambhar <akash.pambhar@yahoo.com>
2023-08-27 17:13:48 +0530

    add student table and chart

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+import { HomeComponent } from './components/home/home.component';
+import { StudentComponent } from './components/student/student.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
